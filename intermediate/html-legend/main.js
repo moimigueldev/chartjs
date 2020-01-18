b3b2dd18c0104ca6b0dc8ef7f1cddd76ebdaa739
moises <moimigueldev@gmail.com>
2020-01-18 17:29:27 -0600

    working on html legend

--- intermediate/html-legend/main.js
+++ intermediate/html-legend/main.js
@@ -7,13 +7,44 @@ var chart = new Chart(ctx, {
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-            label: 'Radar Chart',
+            label: 'First Dataset',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45]
+        },
+        {
+            label: 'Second Dataset',
+            backgroundColor: 'rgb(0, 99, 132)',
+            borderColor: 'rgb(0, 99, 132)',
+            data: [10, 15, 15, 2, 20, 80, 45]
+        },
+        {
+            label: 'Third Dataset',
+            backgroundColor: 'rgb(0, 0, 132)',
+            borderColor: 'rgb(0, 0, 132)',
+            data: [5, 3, 5, 24, 20, 50, 45]
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        legend: {
+            display: true,
+            position: 'left',
+            reverse: false, 
+            labels: {
+                // boxWidth: 5,
+                fontSize: 20,
+            },
+            // onClick: changeFontColor,
+            onHover: changeFontColor,
+        }
+    }
 });
+
+
+function changeFontColor() {
+    chart.options.legend.labels.fontColor = 'red'
+}
+
+document.getElementById('chartjs-legend').innerHTML = chart.generateLegend()
